ea5789ff70c03b694a470c7ccfd075b667998357
NasimLat <nasim-82@mail.ru>
2024-12-22 14:45:23 +0500

    add rename server file

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module calc_service
 
 go 1.23.2
 
-replace github.com/nasimlat/calc_service => ../calc_service
+require github.com/nasimlat/calc_service v0.0.1
 
-require github.com/nasimlat/calc_service v0.0.0-00010101000000-000000000000
+replace github.com/nasimlat/calc_service => ../calc_service
---
+++ server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+    "fmt"
+	"github.com/nasimlat/calc_service/pkg/calc"
+)
+
+type Request struct {
+	Expression string `json:"expression"`
+}
+
+type Response struct {
+	Result string `json:"result"`
+	Error string `json:"error"`
+}
+
+func caclculateHandler(w http.ResponseWriter, r *http.Request) {
+	var req Request
+	var res Response
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		res.Error = err.Error()
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	result, err := calc.Calc(req.Expression)
+	if err != nil {
+		res.Error = err.Error()
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	res.Result = fmt.Sprintf("%f", result)
+	// w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+
+func main() {
+
+	http.HandleFunc("/api/v1/calculate", caclculateHandler)
+	fmt.Println("Server is running on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("internal server error: %v\n", err)
+	}
+}
